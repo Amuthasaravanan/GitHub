bc9988998e4c00d02acd6b95ff19d2624e333ee2
Amuthasaravanan <amuthasaravanan2003@gmail.com>
2022-11-15 17:43:39 +0530

    new file:   index.html 	new file:   test_script.js

---
+++ test_script.js
@@ -0,0 +1,16 @@
+fetch("https://api.thingspeak.com/channels/1913859/feeds.json?results=2").then((data)=>{
+return data.json();
+}).then((objectData)=>{
+console.log(objectData.feeds[0]);
+let tableData= "";
+objectData.feeds.map((values)=>{
+    tableData+= `   <tr>
+    <td>${values.created_at}</td>
+    <td>${values.field3}</td>
+    <td>${values.field1}</td>
+    <td>${values.field2}</td>
+    <td>${values.field4}</td>
+  </tr>`;
+});
+document.getElementById("table_body").innerHTML=tableData
+})
